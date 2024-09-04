829d7f69bfcb91ab47212d4b464ea3b5c3b9f173
Đỗ Phúc Hưng <hsdev.29032004@gmail.com>
2024-09-04 13:28:39 +0700

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 // const API = "https://b421-2402-800-6d3e-95b-fcab-aa23-a446-1558.ngrok-free.app/api"
-const API = "http://localhost:3001/api"
+const API = "https://meelow-backend.vercel.app/1/api"
 
 export const _get = async (path) => {
     const response = await fetch(API + path, {
